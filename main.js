81da2e78ac83f1dcd329f050dd04bdf6ecfca5ee
kennethn23 <kennethng1337@gmail.com>
2024-01-10 09:24:02 -0500

    js and css

--- main.js
+++ main.js
@@ -1,26 +1,92 @@
 const DOMSelectors = {
     content: document.querySelector(".content"),
-    button: document.querySelector(".playButton"),
     rock: document.querySelector("#rock"),
     paper: document.querySelector("#paper"),
     scissors: document.querySelector("#scissors"),
+    historyButton: document.querySelector("#history"),
+    history: document.querySelector(".history"),
 }
+const history = [];
 
+function returnInput (input) {
+    let output;
 
-function rockPaperScissors (input) {
-    const choices = ["Rock", "Paper", "Scissors"];
-    const random = Math.floor(Math.random() * 3);
-    let opponentChoice;
-
-    if (random == 0) {
-        opponentChoice = choices[0];
-    } else if (random == 1) {
-        opponentChoice = choices[1];
-    } else if (random == 2) {
-        opponentChoice = choices[2];
+    if (input == 0) {
+        output = "rock";
+    } else if (input == 1) {
+        output = "paper";
+    } else {
+        output = "scissors";
     }
 
+    return output;
+};
 
+function rockPaperScissors (input) {
+    const random = Math.floor(Math.random() * 3);
+    let opponentChoice;
+    let outcome;
+
+    if (random == 0) {
+        opponentChoice = "rock";
+        if (input == 1) {
+            outcome = "win";
+        } else if (input == 2) {
+            outcome = "lose";
+        } else {
+            outcome = "draw";
+        }
+    } else if (random == 1) {
+        opponentChoice = "paper";
+        if (input == 2) {
+            outcome = "win";
+        } else if (input == 0) {
+            outcome = "lose";
+        } else {
+            outcome = "draw";
+        }
+    } else if (random == 2) {
+        opponentChoice = "scissors";
+        if (input == 0) {
+            outcome = "win";
+        } else if (input == 1) {
+            outcome = "lose";
+        } else {
+            outcome = "draw";
+        }
+    }
+    history.push(outcome.toUpperCase());
+
+    DOMSelectors.history.innerHTML = "";
+    DOMSelectors.content.insertAdjacentHTML("afterbegin", 
+    `<p>Your choice: ${returnInput().toUpperCase()}</p>
+    <p>Opponent choice: ${opponentChoice.toUpperCase()}</p>
+    <h4>${outcome.toUpperCase()}</h4>
+    `)
+};
+
+function getHistory () {
+    DOMSelectors.content.innerHTML = "";
+    DOMSelectors.history.innerHTML = "";
+    let win = 0;
+    let lose = 0;
+    let draw = 0;
+
+    for (let i = 0; i < history.length; i++) {
+        if (history[i] == "WIN") {
+            win++;
+        } else if (history[i] == "LOSE") {
+            lose++;
+        } else {
+            draw++;
+        }
+
+        DOMSelectors.history.insertAdjacentHTML("afterbegin",
+        `<h4>Game ${i + 1}: ${history[i]}</h4>`)
+    }
+
+    DOMSelectors.history.insertAdjacentHTML("afterbegin",
+        `<h2>Wins: ${win} | Losses: ${lose} | Draws: ${draw}</h2>`)
 };
 
 DOMSelectors.rock.addEventListener("click", function () {
@@ -34,3 +100,7 @@ DOMSelectors.paper.addEventListener("click", function () {
 DOMSelectors.scissors.addEventListener("click", function () {
     rockPaperScissors(2);
 });
+
+DOMSelectors.historyButton.addEventListener("click", function () {
+    getHistory();
+})
